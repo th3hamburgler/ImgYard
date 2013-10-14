7aa66377fb711328d0b3df2d97ba1e39ab91ecba
Jim Wardlaw <jim.wardlaw@gmail.com>
2013-10-14 18:16:36 +0100

    Remove log

--- src/Stwt/ImgYard/routes.php
+++ src/Stwt/ImgYard/routes.php
@@ -11,16 +11,9 @@
 
 $controllers = Config::get('img-yard::controllers');
 
-Log::error('got images');
-
 foreach ($controllers as $array) {
     $uri = $array['uri'];
     $model = $array['model'];
-    //$_model = 'img_yard_'.strtolower($model);
     $controller = $array['controller'];
-
-    //Route::model($_model, $model);
-    //Route::get($uri.'/{'.$_model.'}', $controller.'@getImage');
-
     Route::get($uri.'/{id}/{size}', $controller.'@getImage');
 }
